2c234e45497374c6746e3a1f6d41a679deb21534
eladio <sr.eladio@gmail.com>
2014-03-16 19:24:40 +0100

    Añadido el método registerLocalCommandExecutors para asociar el comando global propertiesCommand al executor nuevoLibroExecutor para esta vista.

--- VistaRCP/src/main/java/com/eladio/vistas/StartingAppView.java
+++ VistaRCP/src/main/java/com/eladio/vistas/StartingAppView.java
@@ -4,7 +4,12 @@ import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import org.springframework.richclient.application.PageComponentContext;
 import org.springframework.richclient.application.support.AbstractView;
+import org.springframework.richclient.command.ActionCommandExecutor;
+import org.springframework.richclient.command.support.GlobalCommandIds;
+
+import com.eladio.comandos.NuevoLibroExecutor;
 
 /**
  * @author Carlos Eladio Fernández Ordóñez
@@ -13,6 +18,11 @@ import org.springframework.richclient.application.support.AbstractView;
  */
 public class StartingAppView extends AbstractView {
 
+	private ActionCommandExecutor nuevoLibroExecutor = new NuevoLibroExecutor();
+	
+	/**
+	 * Añade los componentes a la vista
+	 */
 	@Override
 	protected JComponent createControl() {
 		JPanel panel = new JPanel();
@@ -21,5 +31,21 @@ public class StartingAppView extends AbstractView {
 				
 		return panel;
 	}
-
+	
+	/**
+	 * Este método es invocado cuando se inicializa el contenedor.
+	 * 
+	 * Registra en el contexto de la vista la implementación específica del
+	 * comando global 'properties' que queremos asociar a la vista.
+	 * 
+	 * Los ID's de los comandos globales predefinidos están declarados en la
+	 * clase: GlobalCommandIds
+	 * 
+	 * @param context Contexto de la aplicación (Vista actual).
+	 */
+	@Override
+	protected void registerLocalCommandExecutors(PageComponentContext context) {
+		// Asociamos el nuevo ejecutor al comando
+		context.register(GlobalCommandIds.PROPERTIES, this.nuevoLibroExecutor);
+	}
 }
