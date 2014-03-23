00ff2e88bfcb57fff5dc62a25b6801012ac44e81
eladio <sr.eladio@gmail.com>
2014-03-23 19:21:51 +0100

    Modificado el método createControl() para que cree un TableFormBuilder con las propiedades del LibroBean con el que trabaje.

--- VistaRCP/src/main/java/com/eladio/formularios/LibroForm.java
+++ VistaRCP/src/main/java/com/eladio/formularios/LibroForm.java
@@ -1,15 +1,23 @@
 package com.eladio.formularios;
 
 import javax.swing.JComponent;
-import javax.swing.JPanel;
 
 import org.springframework.richclient.form.AbstractForm;
+import org.springframework.richclient.form.binding.BindingFactory;
+import org.springframework.richclient.form.builder.TableFormBuilder;
 
 import com.eladio.beans.LibroBean;
 
 /**
  * Formulario que da soporte al diálogo <code>LibroPropertiesDialog</code> 
  * de la vista <code>VerLibrosVista</code>.
+ * 
+ * AbstractForm es la base de implementación de los formularios.
+ * Implementa los comandos:
+ * 	
+ * 	- CommitCommand: vuelca los datos al bean que se está editando.
+ * 	- RevertCommand: carga el formulario con los datos del bean.
+ * 	- NewFormObjectCommand: crea una nueva instancia del modelo.
  * 
  * @author Carlos Eladio Fernández Ordóñez
  */
@@ -25,9 +33,43 @@ public class LibroForm extends AbstractForm {
 		super.setId("libro");
 	}
 	
+	/**
+	 * Para crear un componente para un formulario, se utilizan lo <code>FormBuilder's</code>.
+	 * Estos crean automáticamente la representación swing del formulario.
+	 * Tres tipos de FormBuilder's:
+	 * 
+	 * 	- GridBagLayoutFormBuilder
+	 *  - HtmlFormBuilder
+	 *  - TableFormBuilder
+	 */
 	@Override
 	protected JComponent createFormControl() {
-		return new JPanel();
+		// El bindingFactory genera enlaces (bindings) con las clases del modelo 
+		// que tiene enlazado el formulario, en este caso el LibroBean libro.
+		BindingFactory bindingFactory = this.getBindingFactory();
+		
+		// Instanciamos el TableFormBuilder
+		TableFormBuilder formBuilder= new TableFormBuilder(bindingFactory);
+		
+		// Añadimos un separador para notificar que empieza la sección de datos del libro.
+		formBuilder.addSeparator("General");
+		
+		// Añadimos un salto de línea.
+		formBuilder.row();
+		
+		// Añadimos las propiedades del bean. 
+		formBuilder.add("titulo");
+		formBuilder.add("autor");
+		formBuilder.row();
+		formBuilder.add("precio");
+		formBuilder.row();
+		formBuilder.addSeparator("Editorial");
+		formBuilder.row();
+		formBuilder.add("editorial.nombre");
+		formBuilder.add("editorial.pais");
+		
+		// Una vez configurado el FormBuilder, se retorna el objeto de tipo JComponent.
+		return formBuilder.getForm();
 	}
 
 }
