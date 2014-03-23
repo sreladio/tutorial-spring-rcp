9965b76a97ff1ebe74aec0da339b76bf6d1e7575
eladio <sr.eladio@gmail.com>
2014-03-23 20:28:19 +0100

    Modificado para que reciba por inyección una instancia del bean UsuariosBeans. Añadido el método onPostStartup() para que muestre el diálogo de login.

--- VistaRCP/src/main/java/com/eladio/aop/ApplicationLifecycleAdvisor.java
+++ VistaRCP/src/main/java/com/eladio/aop/ApplicationLifecycleAdvisor.java
@@ -1,6 +1,11 @@
 package com.eladio.aop;
 
 import org.springframework.richclient.application.config.DefaultApplicationLifecycleAdvisor;
+import org.springframework.richclient.command.support.ExitCommand;
+
+import com.eladio.beans.UsuarioBean;
+import com.eladio.beans.UsuariosBean;
+import com.eladio.dialogos.LoginDialog;
 
 /**
  * @author Carlos Eladio Fernández Ordóñez
@@ -10,7 +15,33 @@ import org.springframework.richclient.application.config.DefaultApplicationLifec
  */
 public class ApplicationLifecycleAdvisor extends DefaultApplicationLifecycleAdvisor {
 
+	private UsuariosBean usuarios;
+	
 	public ApplicationLifecycleAdvisor() {
 		super();
+	}
+	
+	public UsuariosBean getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(UsuariosBean usuarios) {
+		this.usuarios = usuarios;
+	}
+	
+	@Override
+	public void onPostStartup() {
+		System.out.println("Inicializando");
+		UsuarioBean usuario = new UsuarioBean();
+    	new LoginDialog(usuario).showDialog();
+    	System.out.println(usuario.getUser()+","+usuario.getPassword());
+    	
+    	for ( UsuarioBean var:usuarios.getUsuarios())
+    	{
+    		if ( usuario.getUser().equals(var.getUser())&& usuario.getPassword().equals(var.getPassword()))
+    			return;
+    	}
+    	
+    	new ExitCommand().execute();	
 	}
 }
